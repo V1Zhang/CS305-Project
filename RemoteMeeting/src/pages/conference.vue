5cfdf96e2115794cb33e04e0314a2053a3f76cee
Wells <12212108@mail.sustech.edu.cn>
2024-12-27 03:32:29 +0800

    what change

--- RemoteMeeting/src/pages/conference.vue
+++ RemoteMeeting/src/pages/conference.vue
@@ -75,7 +75,7 @@
     data() {
       return {        
         API_URL: 'http://127.0.0.1:7777',
-        IP_URL: 'http://10.32.98.215:7000',
+        IP_URL: 'http://10.32.68.67:7000',
         socket: null,
         store: useMainStore(),
         fallbackUrl: img,
